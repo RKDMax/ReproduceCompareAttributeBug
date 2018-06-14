fb5f1049bfa284b33b89ce6183c1325ed743c73c
Max Quagliotto <mquagliotto@rkd.ca>
2018-06-14 15:34:42 -0400

    Initial Commit

---
+++ Pages/ThisDoesNotWork.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReproduceCompareAttributeBug.Pages
+{
+
+    public class ThisDoesNotWorkModel : PageModel
+    {
+
+        [BindProperty, Required]
+        public string Name { get; set; }
+
+        [BindProperty, Display(Name = "Confirm Name"), Compare("Name", ErrorMessage = "Names must match!")]
+        public string ConfirmName { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Pages/ThisWorks.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReproduceCompareAttributeBug.Pages
+{
+
+    public class ViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Confirm Name"), Compare("Name", ErrorMessage = "Names must match!")]
+        public string ConfirmName { get; set; }
+    }
+
+    public class ThisWorksModel : PageModel
+    {
+
+        [BindProperty]
+        public ViewModel ViewModel { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
